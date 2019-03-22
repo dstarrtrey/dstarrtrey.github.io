9c0cd79a7eca8679d0caabdaa0a620b71da6dfc7
dstarrtrey <dstarrtrey@gmail.com>
2019-03-21 19:00:24 -0700

    Removes load blocking

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
-const loadPage = link => {
-  const data = link.target.getAttribute('data-href');
-  console.log(data);
-  const url = link.target.href;
-  console.log(url);
-  // $(".content").addClass("animate-out");
-  // setTimeout(function() {
-  requestContent(url);
-  history.pushState(data, null, url);
-  // }, 1000);
-};
-const requestContent = file => {
-  $('.content').load(file + ' .content');
-};
+// const loadPage = link => {
+//   const data = link.target.getAttribute('data-href');
+//   console.log(data);
+//   const url = link.target.href;
+//   console.log(url);
+//   // $(".content").addClass("animate-out");
+//   // setTimeout(function() {
+//   requestContent(url);
+//   history.pushState(data, null, url);
+//   // }, 1000);
+// };
+// const requestContent = file => {
+//   $('.content').load(file + ' .content');
+// };
 
 $(document).on('click', '.nav-link', function(event) {
   event.preventDefault();
   $('.homepage').addClass('selected');
   $('.homepage-img').addClass('homepage-img-animate');
   $('.bar').addClass('selected');
-  loadPage(event);
+  // loadPage(event);
 });
